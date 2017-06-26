8e5548fe3b3c2b7d5ab72631af0fa36dd778be69
Bhanu <manthrybhanu@gmail.com>
2017-06-26 18:59:13 +0530

    Create walking girl.

--- js/states/spriteAnimationState.js
+++ js/states/spriteAnimationState.js
@@ -5,6 +5,7 @@ states.spriteAnimationState.prototype = (function() {
     var state = this;
     var preload = function() {
         game.load.spritesheet('earth', 'img/earth.png', 213, 160, 12);
+        game.load.spritesheet('girl', 'img/girl_sprite_sheet.png', 128, 128, 37);
     };
 
     var create = function() {
@@ -16,6 +17,12 @@ states.spriteAnimationState.prototype = (function() {
         // play(name, frameRate, loop, killOnComplete)
         earth.animations.play('rotate', 15, true);
         earth.smoothed = true;
+
+        var girl = game.add.sprite(100, centerY - 100, 'girl');
+        girl.animations.add('walk');
+        girl.animations.play('walk', 20, true);
+        girl.smoothed = true;
+        game.add.tween(girl).to({x: 1000}, 18000, Phaser.Easing.Linear.None, true, 0, -1, true);
 
         var text = game.add.text(10, 500, 'hello', {fill: 'white', fontSize: '70px'});
 
